aec72086bbbef28a1c3d211a1ae9fe3ca848637a
Dylan Bui <dthtb85@gmail.com>
2023-12-14 19:37:20 +1030

    trying to implement quit button

--- mainpainter.py
+++ mainpainter.py
@@ -2,6 +2,7 @@ import pyautogui
 import keyboard
 import random
 from scipy.spatial import ConvexHull
+import sys
 
 # size of 1900 x 860 MS Paint canvas (minus 5)
 xmin = 10
@@ -13,27 +14,27 @@ coords = []
 
 # drags a circle of radius 5 around cursor point
 def circle_draw():
-    pyautogui.move(0, -10)
-    limitx = 5
+    pyautogui.move(0, -8)
+    limitx = 6
 
     x = limitx
     y = 0
 
     while not (x == -limitx and y == 0):
         pyautogui.drag(x, y, 0, button="left")
-        x -= 1
+        x -= 2
         if x >= 0:
-            y += 1
+            y += 2
         else:
-            y -= 1
+            y -= 2
 
     while not (x == limitx and y == 0):
         pyautogui.drag(x, y, 0, button="left")
-        x += 1
+        x += 2
         if x <= 0:
-            y -= 1
+            y -= 2
         else:
-            y += 1
+            y += 2
 
 # random x y coords for cursor within canvas space and return location
 def random_cursor():
@@ -55,8 +56,11 @@ def compute_conv_hull(a):
 
 # draws line connecting points in the drawing order
 def connect_points():
-    edge_points = compute_conv_hull(coords)
-    edge_points.append(edge_points[0])      # adds last point to loop convex hull
+    if len(coords) > 2:
+        edge_points = compute_conv_hull(coords)
+        edge_points.append(edge_points[0])      # adds last point to loop convex hull
+    else:
+        edge_points = coords
     length = len(edge_points)
 
     for i in range(length):
@@ -65,21 +69,29 @@ def connect_points():
         else:
             pyautogui.dragTo(edge_points[i][0], edge_points[i][1], duration=1, button="left")
 
+def test(e):
+    if e.name == "esc":
+        print("Successful termination")
+        # terminate script
+
+keyboard.hook(test)
+
 while True:
     try:
         input = keyboard.read_key()
-        if input.isnumeric():
+        if keyboard.is_pressed("ctrl+1"):
+            coords.sort()       # sorts based on x-coord and then y-coord
+            connect_points()
+            break
+        elif keyboard.is_pressed("z"):
+            cursor_position()
+        elif input.isnumeric():
             num = int(input)
             while num > 0:
                 coords.append(random_cursor())
                 cursor_position()
                 circle_draw()
                 num -= 1
-        if input == "`":
-            cursor_position()
-        if input == "-":
-            coords.sort()       # sorts based on x-coord and then y-coord
-            connect_points()
-            break
     except:
+        print("Input failed")
         break
